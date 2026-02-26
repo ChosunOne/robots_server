9981caa90e1f5cb0c9d96064d5a08c71caa58c57
ChosunOne <chosunone@protonmail.com>
2026-02-26 14:31:36 -0800

    feat: add robots server

--- src/server.rs
+++ src/server.rs
@@ -1,38 +1,46 @@
 use tonic::{Request, Response, Status, transport::Server};
 
-use hello_world::greeter_server::{Greeter, GreeterServer};
-use hello_world::{HelloReply, HelloRequest};
+use robots::{
+    GetRobotsRequest, GetRobotsResponse,
+    robots_service_server::{RobotsService, RobotsServiceServer},
+};
 
-pub mod hello_world {
-    tonic::include_proto!("helloworld");
+pub mod robots {
+    tonic::include_proto!("robots");
 }
 
-#[derive(Debug, Default)]
-pub struct MyGreeter {}
+pub struct RobotsServer;
 
 #[tonic::async_trait]
-impl Greeter for MyGreeter {
-    async fn say_hello(
+impl RobotsService for RobotsServer {
+    async fn get_robots_txt(
         &self,
-        request: Request<HelloRequest>,
-    ) -> Result<Response<HelloReply>, Status> {
-        println!("Got a request: {:?}", request);
-
-        let reply = HelloReply {
-            message: format!("Hello {}!", request.into_inner().name),
+        request: Request<GetRobotsRequest>,
+    ) -> Result<Response<GetRobotsResponse>, Status> {
+        let req = request.into_inner();
+        println!("Got request: {req:?}");
+        let response = GetRobotsResponse {
+            target_url: req.url,
+            robots_txt_url: "https://example.com/robots.txt".to_string(),
+            access_result: 1,
+            http_status_code: 200,
+            groups: vec![],
+            sitemaps: vec![],
+            content_length_bytes: 0,
+            truncated: false,
         };
 
-        Ok(Response::new(reply))
+        Ok(Response::new(response))
     }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
-    let greeter = MyGreeter::default();
+    let service = RobotsServer;
 
     Server::builder()
-        .add_service(GreeterServer::new(greeter))
+        .add_service(RobotsServiceServer::new(service))
         .serve(addr)
         .await?;
 
